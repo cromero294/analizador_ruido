946e68c8e400d8713334c9db942cecad83b65f09
Mario <mar294cr@gmail.com>
2019-01-19 18:35:47 +0100

    Extrapolable a varias clases

--- Clasificador.py
+++ Clasificador.py
@@ -58,6 +58,32 @@ class Clasificador:
 #                              cambiar clases                                  #
 #                                                                              #
 ################################################################################
+  def cambiarClase_variasClases(self, datos, perc=0.5):
+    '''
+    Funcion que hace un swap de la clase al porcentaje indicado del total de
+    ejemplos de la base de datos y, una vez realizado lo dicho, genera una nueva
+    clase comparando cada clase original con el nuevo conjunto de datos (con ruido aleatorio)
+    que indica con un 1 si no ha cambiado y con un 0 si la clase ha cambiado.
+
+    Los nuevos datos se guardan en la variable datos_clases_cambiadas de la clase Datos.
+
+    @param perc: indica el porcentaje de datos que se van a modificar.
+    '''
+    numDatos = datos.shape[0]
+    porcentaje = int(numDatos * perc)
+    clases = datos[:,-1].copy() # TODO: Puede que se pueda quitar el copy
+    datos_nuevos = datos.copy() # TODO: Puede que se pueda quitar el copy
+
+    arrayAleatorio = range(0, numDatos)
+
+    shuffle(arrayAleatorio)
+
+    for num in arrayAleatorio[:porcentaje]:
+        datos_nuevos[num,-1] = 1 - datos_nuevos[num,-1]
+
+    clase_bien_mal_clasificado = [(1.0 if datos_nuevos[i,-1] == clases[i] else 0.0) for i in range(0,numDatos)]
+
+    return np.column_stack((datos_nuevos, np.array(clase_bien_mal_clasificado)))
 
   def cambiarClase(self, datos, perc=0.5):
     '''
