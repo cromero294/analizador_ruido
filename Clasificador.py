c93cfb9d28d5f85ea441561684b7cd07eaed7a11
Mario <mar294cr@gmail.com>
2019-01-20 12:50:08 +0100

    falta probar el de varias_clases

--- Clasificador.py
+++ Clasificador.py
@@ -58,16 +58,9 @@ class Clasificador:
 #                              cambiar clases                                  #
 #                                                                              #
 ################################################################################
-  def cambiarClase_variasClases(self, datos, perc=0.5):
+  def cambiarClase_variasClases(self, datos, diccionario, perc=0.5):
     '''
-    Funcion que hace un swap de la clase al porcentaje indicado del total de
-    ejemplos de la base de datos y, una vez realizado lo dicho, genera una nueva
-    clase comparando cada clase original con el nuevo conjunto de datos (con ruido aleatorio)
-    que indica con un 1 si no ha cambiado y con un 0 si la clase ha cambiado.
 
-    Los nuevos datos se guardan en la variable datos_clases_cambiadas de la clase Datos.
-
-    @param perc: indica el porcentaje de datos que se van a modificar.
     '''
     numDatos = datos.shape[0]
     porcentaje = int(numDatos * perc)
@@ -79,7 +72,11 @@ class Clasificador:
     shuffle(arrayAleatorio)
 
     for num in arrayAleatorio[:porcentaje]:
-        datos_nuevos[num,-1] = 1 - datos_nuevos[num,-1]
+        random_key = random.choice(list(diccionario))
+        while random_key == datos_nuevos[num,-1]:
+            random_key = random.choice(list(diccionario))
+
+        datos_nuevos[num,-1] = diccionario[random_key]
 
     clase_bien_mal_clasificado = [(1.0 if datos_nuevos[i,-1] == clases[i] else 0.0) for i in range(0,numDatos)]
 
