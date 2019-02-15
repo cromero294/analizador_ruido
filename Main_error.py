96449dfb68859f175be97ed5d423220be0eff641
Mario <mar294cr@gmail.com>
2019-02-15 11:34:20 +0100

    Nuevas graficas

--- Main_error.py
+++ Main_error.py
@@ -1,6 +1,70 @@
-z1 = [1, 2, 3, 4]
-z2 = [5, 6, 7, 8]
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 11 16:13:00 2018
 
-error_final = list(map(lambda x, y: x + y, z1, z2))
+@author: Mario Calle Romero
+"""
+from __future__ import division
+from Datos import Datos
+from Clasificador import Clasificador
+from sklearn import tree
+import EstrategiaParticionado
+import numpy as np
+import random
+from sklearn.datasets import make_moons
+import matplotlib.pyplot as plt
 
-print error_final
+try:
+    '''
+    En este Main se prueba la clasificacion de ruido con respecto a la clase original.
+    Que obtengamos 0 a la hora de predecir significa que es ruido.
+
+    El score se mide suponiendo que el dataset no tiene nada de ruido.
+    '''
+    cambiaClase = Clasificador()
+
+    dataset=Datos('Datasets/wdbc.data')
+
+    estrategia = EstrategiaParticionado.ValidacionSimple(1, 80)
+    cambiaClase = Clasificador()
+
+    particiones = estrategia.creaParticiones(dataset)
+
+    score_final_tree = 0
+
+    iteracion = 1
+
+    print("Iteracion: " + str(iteracion))
+
+    datostrain = dataset.extraeDatos(particiones[0].getTrain())
+
+    num_trees = 100
+    tasas_error = [0. for x in range(num_trees)]
+
+    for i in range(100):
+        print "Iteracion " + str(i+1) + "/100"
+
+        particiones = estrategia.creaParticiones(dataset)
+        datostest = dataset.extraeDatos(particiones[0].getTest())
+
+        clfTree,_ = cambiaClase.entrenamiento(datostrain, num_trees, 0.5)
+
+        clfTree.predict_and_save(datostest)
+
+        for j in range(1,num_trees+1,2):
+            print "\tNumero arboles " + str(j) + "/100"
+            tasas_error[j-1] += 1 - clfTree.score_and_save(datostest, j)
+
+    error_final = list(map(lambda x: x/num_trees, tasas_error))
+
+    print "Tasa de error: " + str(error_final)
+
+    plt.plot(range(1,101,2),error_final[::2])
+    #plt.show()
+    plt.title("Error - Clasificadores")
+    plt.savefig("Imagenes/wdbc_8020_impares.eps")
+
+    #print clf.predict([[17.99,10.38,122.8,1001,0.1184,0.2776,0.3001,0.1471,0.2419,0.07871,1.095,0.9053,8.589,153.4,0.006399,0.04904,0.05373,0.01587,0.03003,0.006193,25.38,17.33,184.6,2019,0.1622,0.6656,0.7119,0.2654,0.4601,0.1189,1.0],
+    #                   [13.05,19.31,82.61,527.2,0.0806,0.03789,0.000692,0.004167,0.1819,0.05501,0.404,1.214,2.595,32.96,0.007491,0.008593,0.000692,0.004167,0.0219,0.00299,14.23,22.25,90.24,624.1,0.1021,0.06191,0.001845,0.01111,0.2439,0.06289,0.0]])
+except ValueError as e:
+    print e
--- Main_mesh.py
+++ Main_mesh.py
@@ -28,7 +28,7 @@ try:
     dataset=Datos('Datasets/example4.data')
 
     num_particiones = 10
-    num_arboles = 100
+    num_arboles = 1
 
     #estrategia = EstrategiaParticionado.ValidacionCruzada(num_particiones)
     estrategia = EstrategiaParticionado.ValidacionSimple(1, 80)
@@ -38,12 +38,19 @@ try:
 
     score_final_tree = 0
 
-    iteracion = 1
-
-    print("Iteracion: " + str(iteracion))
-
     datostrain = dataset.extraeDatos(particiones[0].getTrain())
     datostest = dataset.extraeDatos(particiones[0].getTest())
+
+    ###############################
+    #########    MOONS    #########
+    ###############################
+    '''
+    X,y=make_moons(n_samples=500, shuffle=True, noise=0.5, random_state=None)
+    datostrain = np.column_stack((X, y))
+
+    Xt,yt=make_moons(n_samples=1000, shuffle=True, noise=0.5, random_state=None)
+    datostest = np.column_stack((Xt, yt))
+    '''
 
     clfTree,datos_cambiados = cambiaClase.entrenamiento(datostrain, num_arboles, 0.5)
 
@@ -62,7 +69,7 @@ try:
     plotModel(datostest[:,0],datostest[:,1],np.array(clfTree.predict(datostest)),clfTree,"",dataset.getDiccionarios())
 
     #plt.show()
-    plt.savefig("Imagenes/example4_mesh100.eps")
+    plt.savefig("Imagenes/moons_mesh1.eps")
 
     #print clf.predict([[17.99,10.38,122.8,1001,0.1184,0.2776,0.3001,0.1471,0.2419,0.07871,1.095,0.9053,8.589,153.4,0.006399,0.04904,0.05373,0.01587,0.03003,0.006193,25.38,17.33,184.6,2019,0.1622,0.6656,0.7119,0.2654,0.4601,0.1189,1.0],
     #                   [13.05,19.31,82.61,527.2,0.0806,0.03789,0.000692,0.004167,0.1819,0.05501,0.404,1.214,2.595,32.96,0.007491,0.008593,0.000692,0.004167,0.0219,0.00299,14.23,22.25,90.24,624.1,0.1021,0.06191,0.001845,0.01111,0.2439,0.06289,0.0]])
